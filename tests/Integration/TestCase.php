f44c8016013d1985cb70ec7ba1ae62bea5acae64
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-02-10 02:01:50 -0800

    Updates code style.

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -5,4 +5,6 @@ namespace DayRev\Smmry\Tests\Integration;
 /**
  * @see http://docs.mockery.io/en/latest/reference/phpunit_integration.html
  */
-class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase {}
+class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
+{
+}
