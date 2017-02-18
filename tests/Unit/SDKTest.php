5e39e01949d1f0fb4f68d007289a9c025780e595
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-02-17 20:56:04 -0800

    Moves sdk instance to a reusable property.

--- tests/Unit/SDKTest.php
+++ tests/Unit/SDKTest.php
@@ -6,14 +6,19 @@ use DayRev\Smmry\SDK;
 
 class SDKTest extends \PHPUnit_Framework_TestCase
 {
+    protected $sdk;
+
+    public function setUp()
+    {
+        $this->sdk = new SDK(['api_key' => 'D74KLJ345UH9SHDF1', 'summary_length' => 5]);
+    }
+
     public function testContainsDynamicProperties()
     {
-        $sdk = new SDK(['api_key' => 'D74KLJ345UH9SHDF1', 'summary_length' => 5]);
+        $this->assertObjectHasAttribute('api_key', $this->sdk);
+        $this->assertAttributeEquals('D74KLJ345UH9SHDF1', 'api_key', $this->sdk);
 
-        $this->assertObjectHasAttribute('api_key', $sdk);
-        $this->assertAttributeEquals('D74KLJ345UH9SHDF1', 'api_key', $sdk);
-
-        $this->assertObjectHasAttribute('summary_length', $sdk);
-        $this->assertAttributeEquals(5, 'summary_length', $sdk);
+        $this->assertObjectHasAttribute('summary_length', $this->sdk);
+        $this->assertAttributeEquals(5, 'summary_length', $this->sdk);
     }
 }
