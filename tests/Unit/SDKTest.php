8443ccce79c247d3667c32ba1e797dbd545c159f
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-02-17 20:56:28 -0800

    Adds test coverage for protected sdk methods.

--- tests/Unit/SDKTest.php
+++ tests/Unit/SDKTest.php
@@ -3,6 +3,8 @@
 namespace DayRev\Smmry\Tests\Unit;
 
 use DayRev\Smmry\SDK;
+use ReflectionMethod;
+use stdClass;
 
 class SDKTest extends \PHPUnit_Framework_TestCase
 {
@@ -20,5 +22,37 @@ class SDKTest extends \PHPUnit_Framework_TestCase
 
         $this->assertObjectHasAttribute('summary_length', $this->sdk);
         $this->assertAttributeEquals(5, 'summary_length', $this->sdk);
+    }
+
+    public function testCleanSummary()
+    {
+        $summary = new stdClass();
+        $summary->sm_api_title = ' Greeting ';
+        $summary->sm_api_content = ' Hello, how are you? ';
+        $summary->sm_api_img_url = ' http://smmry.com/sm_images/sm_logo.png ';
+
+        $summary = $this->invokeProtectedSdkMethod('cleanSummary', [$summary]);
+
+        $this->assertEquals('Greeting', $summary->sm_api_title);
+        $this->assertEquals('Hello, how are you?', $summary->sm_api_content);
+        $this->assertEquals(' http://smmry.com/sm_images/sm_logo.png ', $summary->sm_api_img_url);
+    }
+
+    public function testBuildUrl()
+    {
+        $url = $this->invokeProtectedSdkMethod('buildUrl', [['SM_QUOTE_AVOID' => 1]]);
+
+        $this->assertEquals(
+            'http://api.smmry.com?SM_API_KEY=D74KLJ345UH9SHDF1&SM_LENGTH=5&SM_QUOTE_AVOID=1',
+            $url
+        );
+    }
+
+    protected function invokeProtectedSdkMethod(string $method, array $args = [])
+    {
+        $reflection = new ReflectionMethod(get_class($this->sdk), $method);
+        $reflection->setAccessible(true);
+
+        return $reflection->invokeArgs($this->sdk, $args);
     }
 }
