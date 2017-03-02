51150bdab552aa0974f4995bce5e2bd073e60d28
Daniel Sposito <daniel.g.sposito@gmail.com>
2017-03-02 00:48:01 -0800

    Removes mockery references that are no longer needed.

--- tests/Integration/TestCase.php
+++ tests/Integration/TestCase.php
@@ -5,7 +5,7 @@ namespace DayRev\Smmry\Tests\Integration;
 /**
  * @see http://docs.mockery.io/en/latest/reference/phpunit_integration.html
  */
-class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
+class TestCase extends \PHPUnit_Framework_TestCase
 {
     protected $config = [];
 
